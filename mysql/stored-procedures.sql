25661a8e6e995ad587fe0d2e8c43b29e74b854dc
briancatraguna-gc <brian.catraguna@gc.com>
2023-10-04 23:53:42 -0400

    Added more stored procedures

--- mysql/stored-procedures.sql
+++ mysql/stored-procedures.sql
@@ -7,8 +7,34 @@ create procedure USP_UpsertCustomer
 	in cust_name VARCHAR(100), in segment int, in email varchar(30), in `password` varchar(50)
 )
 begin
+declare cust_id VARCHAR(20);
+set cust_id = left(uuid(), 20);
 insert into pkbc_customer (cust_id, cust_name, segment, email, `password`)
-values (left(uuid(), 20), cust_name, segment, email, `password`);
+values (cust_id, cust_name, segment, email, `password`);
+select cust_id;
 end$$
 
 delimiter ;
+
+drop procedure if exists USP_GetCustomerById;
+delimiter $$
+create procedure USP_GetCustomerById(
+	in cust_id VARCHAR(20)
+)
+begin
+select * from pkbc_customer a where a.cust_id = cust_id limit 1;
+end$$
+
+delimiter ;
+
+drop procedure if exists USP_GetCustomerByEmail;
+delimiter $$
+create procedure USP_GetCustomerByEmail(
+	in email VARCHAR(30)
+)
+begin
+select * from pkbc_customer a where a.email = email limit 1;
+end$$
+
+delimiter ;
+    
